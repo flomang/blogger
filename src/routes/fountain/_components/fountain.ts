ca2bc95872f5d8ac8d0b428ad0d46467355ba216
flomang <inbox.733@gmail.com>
2022-02-22 17:47:00 -0700

    Naming some vars in fountain with example charity logo.

--- src/routes/fountain/_components/fountain.ts
+++ src/routes/fountain/_components/fountain.ts
@@ -5,7 +5,7 @@ export class Fountain {
     app: PIXI.Application;
     clientID: String = uuid.v1();
     displacementSprite: PIXI.Sprite;
-    board: PIXI.Sprite;
+    heart: PIXI.Sprite;
 
     constructor({ canvas: canvasElement }) {
         let padding = 33;
@@ -17,20 +17,26 @@ export class Fountain {
             backgroundAlpha: 0.0,
             resolution: 1
         });
-        const board = PIXI.Sprite.from("static/heartchakra.png");
-        board.anchor.set(0.5);
-        board.x = this.app.screen.width / 2;
-        board.y = this.app.screen.height / 2;
-        board.height = this.app.screen.height;
-        board.width = this.app.screen.height;
-        this.app.stage.addChild(board);
-        this.board = board;
 
-        // const water = PIXI.Sprite.from("static/watertexture.png");
-        // water.anchor.set(0.5);
-        // water.height = this.app.screen.height - padding;
-        // water.width = this.app.screen.height - padding;
-        // board.addChild(water);
+        let main = new PIXI.Container();
+        this.app.stage.addChild(main);
+
+        const heart = PIXI.Sprite.from("static/heartchakra.png");
+        heart.anchor.set(0.5);
+        heart.x = this.app.screen.width / 2;
+        heart.y = this.app.screen.height / 2;
+        heart.height = this.app.screen.height;
+        heart.width = this.app.screen.height;
+        main.addChild(heart);
+        this.heart = heart;
+
+        const charity = PIXI.Sprite.from("static/humane_society.png");
+        charity.anchor.set(0.5);
+        charity.x = this.app.screen.width / 2;
+        charity.y = this.app.screen.height / 2;
+        charity.height = 233;
+        charity.width = 333;
+        main.addChild(charity);
 
         const displacementSprite = PIXI.Sprite.from('static/displacement_map_repeat.jpeg');
         // Make sure the sprite is wrapping.
@@ -38,12 +44,12 @@ export class Fountain {
 
         const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
         displacementFilter.padding = 10;
-        displacementSprite.position = board.position;
+        displacementSprite.position = heart.position;
         displacementFilter.scale.x = 60;
         displacementFilter.scale.y = 70;
 
         this.app.stage.addChild(displacementSprite);
-        board.filters = [displacementFilter];
+        heart.filters = [displacementFilter];
         this.displacementSprite = displacementSprite;
 
         //Start the game loop
@@ -54,7 +60,7 @@ export class Fountain {
 
     loop(delta: number): void {
         //this.starfield.render(delta);
-        this.board.rotation += delta * 0.0003;
+        this.heart.rotation += delta * 0.0003;
 
         // Offset the sprite position to make vFilterCoord update to larger value. Repeat wrapping makes sure there's still pixels on the coordinates.
         this.displacementSprite.x++;
