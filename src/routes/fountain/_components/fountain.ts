7bf80d40fec2da3198010e54e06d245e49331685
flomang <inbox.733@gmail.com>
2022-02-22 17:21:43 -0700

    The fountain background shall be a rotating heart chakra.

--- src/routes/fountain/_components/fountain.ts
+++ src/routes/fountain/_components/fountain.ts
@@ -5,6 +5,7 @@ export class Fountain {
     app: PIXI.Application;
     clientID: String = uuid.v1();
     displacementSprite: PIXI.Sprite;
+    board: PIXI.Sprite;
 
     constructor({ canvas: canvasElement }) {
         let padding = 33;
@@ -12,17 +13,24 @@ export class Fountain {
         this.app = new PIXI.Application({
             view: canvasElement,
             width: 1024,
-            height: 512 + padding,
+            height: 512,
             backgroundAlpha: 0.0,
             resolution: 1
         });
-        const board = PIXI.Sprite.from("static/fountain.png");
+        const board = PIXI.Sprite.from("static/heartchakra.png");
         board.anchor.set(0.5);
         board.x = this.app.screen.width / 2;
-        board.y = this.app.screen.height / 2 + padding;
-        board.height = this.app.screen.height - padding;
-        board.width = this.app.screen.height - padding;
+        board.y = this.app.screen.height / 2;
+        board.height = this.app.screen.height;
+        board.width = this.app.screen.height;
         this.app.stage.addChild(board);
+        this.board = board;
+
+        // const water = PIXI.Sprite.from("static/watertexture.png");
+        // water.anchor.set(0.5);
+        // water.height = this.app.screen.height - padding;
+        // water.width = this.app.screen.height - padding;
+        // board.addChild(water);
 
         const displacementSprite = PIXI.Sprite.from('static/displacement_map_repeat.jpeg');
         // Make sure the sprite is wrapping.
@@ -46,6 +54,7 @@ export class Fountain {
 
     loop(delta: number): void {
         //this.starfield.render(delta);
+        this.board.rotation += delta * 0.0003;
 
         // Offset the sprite position to make vFilterCoord update to larger value. Repeat wrapping makes sure there's still pixels on the coordinates.
         this.displacementSprite.x++;
