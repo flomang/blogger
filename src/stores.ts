5d8566ef7500ee62d2ae2f764a734d57bb7db373
flomang <inbox.733@gmail.com>
2022-05-06 21:15:15 -0600

    Added UserInfo class for user store.

--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,14 @@
 import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
+
+class UserInfo{
+    avatar_url: string;
+    email: string;
+    id: string;
+    username: string;
+}
+
 // read user from localstorage to persist between page refreshes.
 export const user = writable(browser && (JSON.parse(localStorage.getItem("user")) || "empty"));
-user.subscribe(value => browser && localStorage.setItem("user", JSON.stringify(value)));
+user.subscribe((info: UserInfo) => browser && localStorage.setItem("user", JSON.stringify(info)));
