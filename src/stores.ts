6046f9a37f5303730b97189dcc9ed8d0f9c3921b
flomang <inbox.733@gmail.com>
2022-05-08 19:13:53 -0600

    Class UserInfo should default values to empty string.

--- src/stores.ts
+++ src/stores.ts
@@ -3,12 +3,12 @@ import { writable } from 'svelte/store';
 
 
 class UserInfo{
-    avatar_url: string;
-    email: string;
-    id: string;
-    username: string;
+    avatar_url: string = "";
+    email: string = "";
+    id: string = "";
+    username: string = "";
 }
 
 // read user from localstorage to persist between page refreshes.
-export const user = writable(browser && (JSON.parse(localStorage.getItem("user")) || "empty"));
+export const user = writable(browser && (JSON.parse(localStorage.getItem("user")) || new UserInfo()));
 user.subscribe((info: UserInfo) => browser && localStorage.setItem("user", JSON.stringify(info)));
