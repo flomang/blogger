f5bb7a3baad7ac0207f9836dfba7186768c77738
flo <inbox.733@gmail.com>
2022-06-23 18:59:35 -0600

    The route index should just redirect to transactions. Also removed home from nav.

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -10,9 +10,6 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.url.pathname === "/"}>
-				<a sveltekit:prefetch href="/"> Home </a>
-			</li>
 			<li class:active={$page.url.pathname === "/transactions"}>
 				<a sveltekit:prefetch href="/transactions"> transactions </a>
 			</li>
